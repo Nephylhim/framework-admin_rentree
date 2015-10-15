9b8f9fedb2392cc080c1f3287fd3c5d73d89cac0
Nephylhim <thomas.coussot@laposte.net>
2015-10-15 18:05:03 +0200

    deux premiers dispatchs, a continuer

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-require_once dirname(dirname(dirname(__FILE__))).'/lib/limonade/limonade.php';
+    require_once dirname(dirname(dirname(__FILE__))).'/lib/limonade/limonade.php';
 
-
-yolo
+    dispatch("/", "home.php");
+    dispatch("/home", "home.php");
 
 ?>

2142d4cd6e34ed062fe592f624864c1d0a2c7a76
PCYoshi <josephcaillet@gmail.com>
2015-11-24 14:24:13 +0100

    test php file

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+	echo "<h1>Ca marche! :)</h1>";
+	$this->doSomething();
+echo "hello world";
+echo "<h1>";
+echo "My name is";echo "<span id='the'>toto</span></h1>";
+	if(true == true)
+	{
+		echo "salut les <b>Poteau!</b>";
+		$mavar = 42;
+		echo $mavar;
+
+	}
+?>

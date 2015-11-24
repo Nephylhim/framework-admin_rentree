829d0b1205d61651b14c88090605e372baa1fc2d
Nephylhim <thomas.coussot@laposte.net>
2015-11-24 17:07:46 +0100

    configuration file modified : suppression of unused item

--- views/layouts/inc/config.inc.php
+++ views/layouts/inc/config.inc.php
@@ -1,28 +1,10 @@
 <?php
 	$project_properties = array(
-		 'title'	      => 'Admin rentrée'
-		,'CSS_filename'   => '../style/style.css'
-		,'charset'        => 'utf-8'
-		,'DB_host'        => '127.0.0.1'
+		 'DB_host'        => '127.0.0.1'
 		,'DB_port'        => 'not defined'
 		,'DB_name'        => 'not defined'
 		,'DB_logon'       => 'root'
 		,'DB_pw'          => ''
 		,'ERROR'          => 'Une erreur (inconnue) est survenue !'
-	);
-
-	$project_menu = array(
-        array(
-			 'item' => 'Accueil'
-			,'action' => 'home'
-		)
-        ,array(
-			 'item' => 'Admin Fichiers'
-			,'action' => 'admin_files'
-		)
-		, array(
-			 'item' => 'Admin Données'
-			,'action' => 'admin_datas'
-		)
 	);
 ?>

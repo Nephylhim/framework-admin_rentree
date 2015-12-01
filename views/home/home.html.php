f1b291eb63b814358d68a69224e3f6889b9ecc8b
Nephylhim <thomas.coussot@laposte.net>
2015-12-01 15:55:24 +0100

    start to create home page

--- views/home/home.html.php
+++ views/home/home.html.php
@@ -2,4 +2,34 @@
 
 wololo !!!
 
+<div class="header">
+    <p class="center">
+        <img src="ressources/images/logo_ISEN.png" />
+    </p>
+    <h1 class="center blisen">Documents de rentrée</h1>
+</div>
+
+<div class="body">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+
+            <div class="row">
+
+                <div class="form-line">
+                    <div class="col-sm-6">
+                        Courriel :
+                    </div>
+                    <div class="col-sm-6">
+                        <input type="email">
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
 <?php end_content_for();?>

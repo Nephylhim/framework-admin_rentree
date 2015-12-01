38513c4b266d045d42fe31275472b6e32158fe67
PCYoshi <josephcaillet@gmail.com>
2015-12-01 17:56:11 +0100

    class skell created

--- models/datas/Data.class.php
+++ models/datas/Data.class.php
@@ -1,8 +1,164 @@
 <?php
-/*
-Listage des promos
-Ajout d'une promo
-Suppression d'une promo
-Renommage d'une promo
-*/
+class Data
+{
+	private $id = -1;
+	private $identifiant = "";
+	private $nom_fils = "";
+	private $prenom_fils = "";
+	private $ddn_fils = "";
+	private $tel_mobile = "";
+	private $courriel = "";
+	private $date = "";
+	private $ip = "";
+
+	public function __construct($id = null)
+	{
+
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getIdentifiant()
+	{
+		return $this->identifiant;
+	}
+
+	/**
+	 * @param string $identifiant
+	 */
+	public function setIdentifiant($identifiant)
+	{
+		$this->identifiant = $identifiant;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getNomFils()
+	{
+		return $this->nom_fils;
+	}
+
+	/**
+	 * @param string $nom_fils
+	 */
+	public function setNomFils($nom_fils)
+	{
+		$this->nom_fils = $nom_fils;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPrenomFils()
+	{
+		return $this->prenom_fils;
+	}
+
+	/**
+	 * @param string $prenom_fils
+	 */
+	public function setPrenomFils($prenom_fils)
+	{
+		$this->prenom_fils = $prenom_fils;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDdnFils()
+	{
+		return $this->ddn_fils;
+	}
+
+	/**
+	 * @param string $ddn_fils
+	 */
+	public function setDdnFils($ddn_fils)
+	{
+		$this->ddn_fils = $ddn_fils;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTelMobile()
+	{
+		return $this->tel_mobile;
+	}
+
+	/**
+	 * @param string $tel_mobile
+	 */
+	public function setTelMobile($tel_mobile)
+	{
+		$this->tel_mobile = $tel_mobile;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCourriel()
+	{
+		return $this->courriel;
+	}
+
+	/**
+	 * @param string $courriel
+	 */
+	public function setCourriel($courriel)
+	{
+		$this->courriel = $courriel;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDate()
+	{
+		return $this->date;
+	}
+
+	/**
+	 * @param string $date
+	 */
+	public function setDate($date)
+	{
+		$this->date = $date;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getIp()
+	{
+		return $this->ip;
+	}
+
+	/**
+	 * @param string $ip
+	 */
+	public function setIp($ip)
+	{
+		$this->ip = $ip;
+	}
+
+	public function create()
+	{}
+
+	public function update()
+	{}
+
+	public static function getAllDatas()
+	{}
+}
 ?>
--- models/promos/Promo.class.php
+++ models/promos/Promo.class.php
@@ -1,7 +1,46 @@
 <?php
-/*
-Listage des données
-Modification des données
-Extraction des données
-*/
+class Promo
+{
+	private $promo = "";
+	private $label = "";
+
+	public function __construct($promo = null)
+	{}
+
+	/**
+	 * @return string
+	 */
+	public function getPromo()
+	{
+		return $this->promo;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLabel()
+	{
+		return $this->label;
+	}
+
+	/**
+	 * @param string $label
+	 */
+	public function setLabel($label)
+	{
+		$this->label = $label;
+	}
+
+	public function create()
+	{}
+
+	public function updated()
+	{}
+
+	public function delete()
+	{}
+
+	public static function getAllPromos()
+	{}
+}
 ?>

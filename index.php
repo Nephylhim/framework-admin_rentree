9c9fd4ac52eab1a044e8d9e0ecbf57f939f52bb9
Nephylhim <thomas.coussot@laposte.net>
2015-12-08 17:21:29 +0100

    start the promo panel liste and ajax request

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ dispatch("/", "home");
 dispatch("/home", "home");
 dispatch("/admin_files", "files");
 dispatch("/admin_datas", "datas");
-
+dispatch_get("/promos/get", "getPromos");
 
 run();
 ?>
--- views/home/admin_files.html.php
+++ views/home/admin_files.html.php
@@ -20,12 +20,66 @@
     <div class="row">
         <div class="col-md-6">
 
+            <section class="promotionListPanel">
+                <h1>Liste des promotions</h1>
 
+                <div class="newPromo">
+                    <div class="col-md-6">
+                        <input id="promotionName" class="form-control" placeholder="Nouvelle promotion">
+                    </div>
+                    <div class="col-md-3">
+                        <select id="ANumber" class="form-control">
+                            <option>_A1 </option>
+                            <option>_A2 </option>
+                            <option>_A3 </option>
+                            <option>_A4 </option>
+                            <option>_A5 </option>
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <button id="bouton_AjouterPromo" title="Ajouter une promotion" class="btn generation_element_to_change btn-success" style="display: inline-block;">
+                            Valider
+                        </button>
+                    </div>
+                </div>
+
+                <div id="promotionList">
+
+                </div>
+
+                Test
+                test
+                test
+            </section>
 
         </div>
     </div>
 </div>
 
 
+
+<?php end_content_for();?>
+
+<?php content_for('script'); ?>
+
+<script>
+    /*$.ajax({
+        method: "GET",
+        url: "?/promos/get";
+    }).done( function( content ){
+        $("#promotionList").html("Coucou !!!");
+    });*/
+
+    $(document).ready(function(){
+        console.log( "Appel Ajax" );
+
+        $.ajax({
+            url: "<?=url_for('/promos/get'); ?>",
+            method: "GET"
+        }).success( function($content){
+            $("#promotionList").html($content);
+        });
+    });
+</script>
 
 <?php end_content_for();?>

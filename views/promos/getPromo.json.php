939299d877d34658ff92276680fbf39d8ca86245
PCYoshi <josephcaillet@gmail.com>
2015-12-13 22:00:52 +0100

    Promo class implements json serialisation, for a better view code.

--- views/promos/getPromo.json.php
+++ views/promos/getPromo.json.php
@@ -1,15 +1,3 @@
 {
-	"promos":[
-	<?php
-	$length = count($promos);
-	foreach($promos as $key => $promo)
-	{
-		echo '{"promo": "'.$promo->getPromo().'", "label":"'.$promo->getLabel().'"}';
-		if($key+1 != $length)
-		{
-			echo ",";
-		}
-	}
-	?>
-	]
+	"promos": <?php echo json_encode($promos); ?>
 }

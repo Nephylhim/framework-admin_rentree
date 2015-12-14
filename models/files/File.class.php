bc2ac3e6deede1ab931eb444168180d84666db0b
PCYoshi <josephcaillet@gmail.com>
2015-12-14 18:36:30 +0100

    File class coding

--- models/files/File.class.php
+++ models/files/File.class.php
@@ -1,5 +1,5 @@
 <?php
-class Files implements JsonSerializable
+class File implements JsonSerializable
 {
 	private $id = -1;
 	private $rang = -1;
@@ -88,22 +88,156 @@ class Files implements JsonSerializable
 	}
 
 	public function create()
-	{}
+	{
+		global $bdd;
+
+		$newFile = basename($this->getFichier());
+
+		$ok = move_uploaded_file($this->getFichier(), dirname(__FILE__)."/../../rentree/pdf/".$newFile);
+
+		if(!$ok)
+		{
+			return false;
+		}
+
+		$this->setFichier($newFile);
+
+		$bdd->beginTransaction();
+
+		$sql = "INSERT INTO document(id, rang, promo, libelle, fichier)
+				VALUES (NULL, ?, ?, ?, ?)";
+
+		$rq =  $bdd->prepare($sql);
+
+		$ok = $rq->execute(array(
+			$this->getRang(),
+			$this->getPromo(),
+			$this->getLibelle(),
+			$this->getFichier()
+		));
+
+		if(!$ok)
+		{
+			$bdd->rollBack();
+			return false;
+		}
+
+		$sql = "SELECT LAST_INSERT_ID() AS id";
+
+		$rq = $bdd->prepare($sql);
+
+		$ok = $rq->execute();
+
+		if(!$ok)
+		{
+			$bdd->rollBack();
+			return false;
+		}
+
+		$data = $rq->fetch();
+		$this->setId($data['id']);
+
+		$bdd->commit();
+		return true;
+	}
 
 	public function update()
-	{}
+	{
+		global $bdd;
+		$sql = "UPDATE document
+				SET rang = ?,
+					promo = ?,
+					libelle = ?
+				WHERE id = ?";
+
+		$rq = $bdd->prepare($sql);
+
+		return $rq->execute(array(
+			$this->getRang(),
+			$this->getPromo(),
+			$this->getLibelle(),
+			$this->getId()
+		));
+	}
 
 	public function delete()
-	{}
+	{
+		global $bdd;
 
-	public static function getAllDocumentByPromo($promo)//ORDER BY ...
-	{}
+		$ok = unlink(dirname(__FILE__)."/../../rentree/pdf/".$this->getFichier());
 
-	public static function getAllDocumentsById($id)
-	{}
+		if(!$ok)
+		{
+			return false;
+		}
 
-	public static function deleteAllDocuments()
-	{}
+		$sql = "DELETE FROM document
+				WHERE id = ?";
+
+		$rq = $bdd->prepare($sql);
+
+		return $rq->execute(array(
+			$this->getId()
+		));
+	}
+
+	public static function getAllDocumentByPromo($promo)
+	{
+		global $bdd;
+
+		$sql = "SELECT *
+				FROM document
+				WHERE promo = ?
+				ORDER BY rang";
+
+		$rq = $bdd->prepare($sql);
+
+		$rq->execute(array(
+			$promo
+		));
+
+		$documents = [];
+
+		while($data = $rq->fetch())
+		{
+			$doc = new File();
+			$doc->setId($data["id"]);
+			$doc->setRang($data["rang"]);
+			$doc->setPromo($data["promo"]);
+			$doc->setLibelle($data["libelle"]);
+			$doc->setFichier($data["fichier"]);
+
+			$documents[] = $doc;
+		}
+
+		return $documents;
+	}
+
+	public static function getDocumentById($id)
+	{
+		global $bdd;
+
+		$sql = "SELECT *
+				FROM document
+				WHERE id = ?";
+
+		$rq = $bdd->prepare($sql);
+
+		$rq->execute(array(
+			$id
+		));
+
+		$data = $rq->fetch();
+
+		$doc = new File();
+		$doc->setId($data["id"]);
+		$doc->setRang($data["rang"]);
+		$doc->setPromo($data["promo"]);
+		$doc->setLibelle($data["libelle"]);
+		$doc->setFichier($data["fichier"]);
+
+		return $doc;
+	}
 
 	function jsonSerialize()
 	{

523e350c659671e22340108b2144d221c9534b67
PCYoshi <josephcaillet@gmail.com>
2015-12-15 15:47:53 +0100

    Promo controllers coded and working

--- controllers/PROMO.php
+++ controllers/PROMO.php
@@ -1,13 +1,24 @@
 <?php
 require_once(dirname(__FILE__)."/../models/model_class_includes.php");
 
-function getPromo(){
-    set("promos", Promo::getAllPromos());
+function getPromo()
+{
+    $promo = params("promo");
+	if ($promo == null)
+	{
+		set("promos", Promo::getAllPromos());
+	}
+	else
+	{
+		set("promos", Promo::getPromoByPromo($promo));
+	}
+
     header('Content-Type: application/json');
     return render("promos/getPromo.json.php");
 }
 
-function addPromo(){
+function addPromo()
+{
 	$promo = params("promo");
 	$label = params("label");
 
@@ -18,7 +29,35 @@ function addPromo(){
 
 	set("status", $status);
 
+	header('Content-Type: application/json');
 	return render("status.json.php");
 }
 
+function delPromo()
+{
+	$promo = params("promo");
+
+	$promoToDel = Promo::getPromoByPromo($promo);
+	$status = $promoToDel->delete();
+
+	set("status", $status);
+
+	header('Content-Type: application/json');
+	return render("status.json.php");
+}
+
+function updPromo()
+{
+	$promo = params("promo");
+	$newLabel = params("newLabel");
+
+	$promoToUpd = Promo::getPromoByPromo($promo);
+	$promoToUpd->setLabel($newLabel);
+	$status = $promoToUpd->update();
+
+	set("status", $status);
+
+	header('Content-Type: application/json');
+	return render("status.json.php");
+}
 ?>
--- index.php
+++ index.php
@@ -6,10 +6,11 @@ dispatch("/home", "home");
 dispatch("/admin_files", "files");
 dispatch("/admin_datas", "datas");
 
-dispatch_get("/promos/get", "getPromo");
-dispatch_post("/promos/add/:promo/:label", "addPromo");
-dispatch_post("/promos/del", "delPromo");
-dispatch_post("/promos/upd", "updPromo");
+dispatch_get("/promos/get", "getPromo");//get all promos
+dispatch_get("/promos/get/:promo", "getPromo");//get a promo
+dispatch_post("/promos/add/:promo/:label", "addPromo");//add a promo
+dispatch_post("/promos/del/:promo", "delPromo");//delete a promo
+dispatch_post("/promos/upd/:promo/:newLabel", "updPromo");//update promo
 
 dispatch_get("/files/get", "getFile");
 dispatch_post("/files/add", "addFile");

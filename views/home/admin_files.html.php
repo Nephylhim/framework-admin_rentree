20f663712cb9944824a7785be38a8428d95cac8a
Nephylhim <thomas.coussot@laposte.net>
2015-12-15 15:42:54 +0100

    Keine Problem have been replaced by Ajouter une promotion

--- views/home/admin_files.html.php
+++ views/home/admin_files.html.php
@@ -141,7 +141,7 @@
                         <div class="modal-content">
                           <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                            <h4 class="modal-title" id="myModalLabel">Keine Problem !</h4>
+                            <h4 class="modal-title" id="myModalLabel">Ajouter une promotion</h4>
                           </div>
                           <div class="modal-body">
                               <div class="newPromo row">

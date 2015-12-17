6392e2fb8fa8e2a0b478b3248d4e4caf89e5fa84
PCYoshi <josephcaillet@gmail.com>
2015-12-17 15:12:35 +0100

    trying to solve upload pb v2

--- views/home/uploadPage.html.php
+++ views/home/uploadPage.html.php
@@ -2,39 +2,39 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<link rel='stylesheet' href='css/bootstrap.min.css'>
+		<!--<link rel='stylesheet' href='css/bootstrap.min.css'>-->
 		<script src='js/jquery-2.1.4.min.js'></script>
 		<link href="css/main.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body>
-		<form id="newFileForm" method="post" action="">
+		<form id="newFileForm" method="post" action="" enctype="multipart/form-data">
 				<div class="row">
 					<div class="col-md-5">
 						<label class="control-label">Sélectionnez le fichier :</label>
 					</div>
 					<div class="col-md-5 col-md-offset-1">
-						<input id="input-1" type="file" name="document">
+						<input id="input-1" type="file" name="document" required>
 					</div>
 
 					<div class="col-md-5">
 						<label class="control-label">Libéllé :</label>
 					</div>
 					<div class="col-md-6 col-md-offset-1">
-						<input  id="newFileLibelleInput" class="form-control">
+						<input  id="newFileLibelleInput" class="form-control" required>
 					</div>
 
 					<div class="col-md-5">
 						<label class="control-label">Promo liée :</label>
 					</div>
 					<div class="col-md-6 col-md-offset-1">
-						<input  id="newFilePromoInput" class="form-control">
+						<input  id="newFilePromoInput" class="form-control" required>
 					</div>
 
 					<div class="col-md-5">
 						<label class="control-label">Rang :</label>
 					</div>
 					<div class="col-md-6 col-md-offset-1">
-						<input  id="newFileRankInput" class="form-control" type="number">
+						<input  id="newFileRankInput" class="form-control" type="number" required>
 					</div>
 				</div>
 			<button id="newFileBtn" title="Ajouter un fichier" class="btn btn-success" style="display: inline-block;" data-dismiss="modal" aria-hidden="true" type="submit">Valider</button>
@@ -43,9 +43,9 @@
 			$(function(){
 				$('#newFileForm').submit(function(event){
 					var action = '';
-					action = "<?=url_for('/files/add')?>" + "/" + $("#newFilePromoInput").val() + "/" + + $("#newFileRankInput").val() + "/" + + $("#newFileLibelleInput").val() + "/"
+					action = "<?=url_for('/files/add')?>" + "/" + $("#newFilePromoInput").val() + "/" + $("#newFileRankInput").val() + "/" + $("#newFileLibelleInput").val() + "/"
 					console.log(action);
-					event.preventDefault();
+					//event.preventDefault();
 					$(this).attr('action', action);
 				})});
 		</script>
